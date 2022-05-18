2a0ec9f3fe7cede48db34666b2cb9f949508185a
HafidzMubarok <mubarokhafidz29@gmail.com>
2022-05-18 12:57:21 +0700

    ✨ FE: Feature Login

---
+++ client/src/views/login/components/login.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+const Login=()=>{
+
+    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
+    const avatarStyle={backgroundColor:'#1bbd7e'}
+    const btnstyle={margin:'8px 0'}
+    return(
+        Login
+    )
+}
+
+export default Login;
