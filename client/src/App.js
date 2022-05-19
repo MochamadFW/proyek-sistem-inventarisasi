ab3ff3d79c60b3f9a7e5889a5a203facc0a70a46
HafidzMubarok <mubarokhafidz29@gmail.com>
2022-05-19 14:34:49 +0700

    :lipstick: FE: Feature Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import { Routes, Route, Link } from "react-router-dom";
 
 import Pendataan from './views/pendataan/pages/pendataan';
+import Login from './views/login/pages/login';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Pendataan/>} />
+      <Route path="/" element={<Login/>} />
     </Routes>
   );
 }
