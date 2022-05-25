c55a9524fd8f6b4bb8e534d9a4f4a77e37416d35
gopelkujo <sigantengnoval@gmail.com>
2022-05-25 10:51:10 +0700

    :sparkles: fe: Add new pengguna KIB page

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route, Link } from "react-router-dom";
 
 import Pendataan from './views/pendataan/pages/pendataan';
 import Login from './views/login/pages/login';
+import PenggunaKib from './views/pengguna/kib/pages/penggunaKib'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login/>} />
+      <Route path="/pengguna/kib" element={<PenggunaKib/>} />
     </Routes>
   );
 }
